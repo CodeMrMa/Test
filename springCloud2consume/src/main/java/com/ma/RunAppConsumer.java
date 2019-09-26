cd85b16bed16f0beaaa3a4b5f933d82f758c150f
mawenlaing001 <1147201632@qq.com>
2019-09-26 18:17:18 +0800

    123

--- springCloud2consume/src/main/java/com/ma/RunAppConsumer.java
+++ springCloud2consume/src/main/java/com/ma/RunAppConsumer.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class RunAppConsumer {
 	/**
 	 *
-	 * @param args0123
+	 * @param args01234
 	 */
 	
 	public static void main(String[] args) {
